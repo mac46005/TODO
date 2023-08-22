133ca89deceff1c9ed08592373e99176b0373b39
Marco Preciado <mac46005@gmail.com>
2023-08-22 13:55:08 -0500

    Lets fix this tommorow

--- app/src/main/java/com/preciado/todo/features/home/HomeView.kt
+++ app/src/main/java/com/preciado/todo/features/home/HomeView.kt
@@ -1,9 +1,11 @@
 package com.preciado.todo.features.home
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
@@ -18,10 +20,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.preciado.todo.R
 import com.preciado.todo.core.common_visuals.components.TransparentButton
@@ -29,9 +34,12 @@ import com.preciado.todo.core.views.BaseView
 import com.preciado.todo.data.CRUDEnum
 import com.preciado.todo.features.home.components.ListButton
 import com.preciado.todo.features.home.components.BigMessage
-import com.preciado.todo.features.home.components.TasksLists
+import com.preciado.todo.features.home.components.TODOTasksCollection
+import com.preciado.todo.features.home.components.TaskItem
+import com.preciado.todo.features.home.components.TaskList
 import com.preciado.todo.features.home.core.HomeViewModel
 import com.preciado.todo.ui.theme.TODOTheme
+import java.util.BitSet
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,7 +48,22 @@ fun HomeView(
     navController: NavController
 ) {
 
+
+
     val listId by vm.selectedTODOListId.observeAsState(0)
+
+    val uncompletedTasksViewButton = remember {
+        mutableStateOf(false)
+    }
+    val completedTasksViewButton = remember {
+        mutableStateOf(false)
+    }
+    val uncompletedVisible = remember {
+        mutableStateOf(true)
+    }
+    val completedVisible = remember {
+        mutableStateOf(false)
+    }
 
     TODOTheme {
         Scaffold(
@@ -53,21 +76,38 @@ fun HomeView(
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Row() {
-                            Button(onClick = { /*TODO*/ }) {
-                                Icon(painter = painterResource(id = R.drawable.red_x_light), contentDescription = "Uncompleted List")
+//                            UNCOMPLETED LIST VIEW BUTTON
+                            Button(
+                                onClick = {
+                                    uncompletedVisible.value = uncompletedVisible.value == false
+                                },
+                                enabled = uncompletedTasksViewButton.value
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.baseline_close_24),
+                                    contentDescription = "Uncompleted List"
+                                )
                             }
-                            Button(onClick = { /*TODO*/ }) {
-                                Icon(painter = painterResource(id = R.drawable.green_check_light), contentDescription = "Completed List")
+//                            COMPLETED LIST VIEW BUTTON
+                            Button(
+                                onClick = {
+                                    completedVisible.value = completedVisible.value == false
+                                },
+                                enabled = completedTasksViewButton.value
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.baseline_check_24),
+                                    contentDescription = "Completed List"
+                                )
                             }
-                            Button(onClick = { /*TODO*/ }) {
-                                Icon(painter = painterResource(id = R.drawable.outline_delete_24)   , contentDescription = "Delete List")
+                            Button(
+                                onClick = {
+
+                                }
+                            ) {
+
                             }
                         }
-
-
-
-
-
 
 
                         //Add new task
@@ -86,8 +126,6 @@ fun HomeView(
                 }
             }
         ) {
-
-
             val padding = it
             BaseView {
 
@@ -100,7 +138,6 @@ fun HomeView(
                             onClick =
                             {
                                 vm.onListSelected(todoListId = todoList.id)
-                                Log.d("HomeView", "selected list id: ${listId}")
                             }
                         ) {
                             Text(text = todoList.name)
@@ -113,15 +150,11 @@ fun HomeView(
                     }
                 }
                 Divider()
-
-
-                if(listId == 0){
-                    BigMessage(
-                        message = "Please select a TODO list from above!",
-                        paddingBottom = padding.calculateBottomPadding()
-                    )
+                
+                if(listId != 0){
+                    BigMessage(message = "Select a list")
                 }else{
-                    TasksLists(navController = navController,listId = listId)
+                    BigMessage(message = "")
                 }
             }
         }
