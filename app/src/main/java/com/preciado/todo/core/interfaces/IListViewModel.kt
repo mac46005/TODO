84aac81fbc8e94cd96e45ae481b74876bbfb1c95
Marco Preciado <mac46005@gmail.com>
2023-09-03 19:18:17 -0500

    Creating interfaces so that I can potentially achieve interoperability.

--- app/src/main/java/com/preciado/todo/core/interfaces/IListViewModel.kt
+++ app/src/main/java/com/preciado/todo/core/interfaces/IListViewModel.kt
@@ -1,5 +1,8 @@
 package com.preciado.todo.core.interfaces
 
+import kotlinx.coroutines.flow.Flow
+
 interface IListViewModel<T> : IViewModel<T> {
+    fun loadList(vararg args: Any): Flow<List<T>>
 
 }
