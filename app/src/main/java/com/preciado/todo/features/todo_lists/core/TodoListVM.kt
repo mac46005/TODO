28682312cfa26c263a24bf5aae3ac30ab8c0c8bb
Marco Preciado <mac46005@gmail.com>
2023-09-04 23:46:32 -0500

    Work on navigation on the TodoLists

--- app/src/main/java/com/preciado/todo/features/todo_lists/core/TodoListVM.kt
+++ app/src/main/java/com/preciado/todo/features/todo_lists/core/TodoListVM.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavController
 import com.preciado.todo.core.ui_models.interfaces.IVM
 import com.preciado.todo.core.models.TODOList
 import com.preciado.todo.core.ui_models.models.ListVM
+import com.preciado.todo.data.CRUDEnum
 import com.preciado.todo.data.TODOListsTable
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -28,7 +29,7 @@ class TodoListVM @Inject constructor (
     }
 
     override fun onItemSelected(item: TODOList) {
-        TODO("Not yet implemented")
+        navController!!.navigate("")
     }
 
     override fun onLoad(vararg args: Any) {
@@ -40,7 +41,15 @@ class TodoListVM @Inject constructor (
     }
 
     override fun onNavigateTo(vararg args: Any) {
-        TODO("Not yet implemented")
+        if(args[0] is CRUDEnum){
+            val crudOp = args[0] as CRUDEnum
+            when(crudOp){
+                CRUDEnum.CREATE -> navController!!.navigate("add_edit_list/${crudOp.ordinal}/0")
+                CRUDEnum.READ -> TODO()
+                CRUDEnum.UPDATE -> TODO()
+                CRUDEnum.DELETE -> TODO()
+            }
+        }
     }
 
     override fun setNavigator(navController: NavController) {
