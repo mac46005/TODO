45239c8d11b69ac4430c811b7400dc30a2fa1a23
Marco Preciado <mac46005@gmail.com>
2023-09-07 00:30:34 -0500

    Saving progress so far. Just trying to clean up my code

--- app/src/main/java/com/preciado/todo/core/models/vm_models/interfaces/IFormVM.kt
+++ app/src/main/java/com/preciado/todo/core/models/vm_models/interfaces/IFormVM.kt
@@ -1,5 +1,8 @@
 package com.preciado.todo.core.models.vm_models.interfaces
 
+import com.preciado.todo.data.CRUDEnum
+
 interface IFormVM<T>: IVM<T> {
+    var crudEnum: CRUDEnum
     fun submitForm()
 }
---
+++ app/src/main/java/com/preciado/todo/features/add_edit_list/core/AddEditListFormVM.kt
@@ -0,0 +1,33 @@
+package com.preciado.todo.features.add_edit_list.core
+
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import com.preciado.todo.core.models.app_models.TODOList
+import com.preciado.todo.core.models.vm_models.models.FormVM
+import javax.inject.Inject
+
+class AddEditListFormVM @Inject constructor() : FormVM<TODOList>() {
+
+
+
+
+    override fun onLoad(vararg args: Any) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBackButtonClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateTo(route: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setModel(model: TODOList) {
+        TODO("Not yet implemented")
+    }
+
+    override fun submitForm() {
+        TODO("Not yet implemented")
+    }
+}
